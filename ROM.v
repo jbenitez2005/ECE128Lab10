7f3b5108647bbb7e937224255e2baf6a3f01dc60
jbenitez2005 <146253276+jbenitez2005@users.noreply.github.com>
2025-12-03 00:23:58 -0500

    Add ROM module with address-to-data mapping

---
+++ ROM.v
@@ -0,0 +1,16 @@
+module ROM (ROM_data, ROM_addr);
+output reg [3:0] ROM_data;
+input [2:0] ROM_addr;
+always @(ROM_addr) begin
+	case (ROM_addr)
+		3'd0: ROM_data = 4'b0000;
+		3'd1: ROM_data = 4'b1100;
+		3'd2: ROM_data = 4'b0110;
+		3'd3: ROM_data = 4'b0111;
+		3'd4: ROM_data = 4'b1000;
+		3'd5: ROM_data = 4'b0001;
+		3'd6: ROM_data = 4'b1101;
+		3'd7: ROM_data = 4'b1110;
+	endcase
+end
+endmodule
